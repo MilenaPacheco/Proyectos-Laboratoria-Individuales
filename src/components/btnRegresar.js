4637b37744dfc958b70bf7171d64ee10f1509290
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2024-02-09 00:44:01 -0500

    Se ingresó botón regresar en los componentes, queda objeto de modificación

--- src/components/btnRegresar.js
+++ src/components/btnRegresar.js
@@ -1,1 +1,6 @@
 // regresa una pantalla atrás
+export const btnRegresar = () => {
+  const btnRegresar = document.createElement("button");
+  btnRegresar.className = "btn-regresar";
+  return btnRegresar
+};
