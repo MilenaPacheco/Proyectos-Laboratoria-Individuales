770931fa6fa28001d8bf19a5f952ebf2816364ff
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2024-02-12 11:43:10 -0500

    Se adiciona una clase para darle una ubicación distinta en la vista de Api Key

--- src/components/btnRegresar.js
+++ src/components/btnRegresar.js
@@ -1,7 +1,7 @@
 // regresa una pantalla atrás
 export const btnRegresar = () => {
   const btnRegresar = document.createElement("button");
-  btnRegresar.className = "btn-regresar";
+  btnRegresar.className = "btn-regresar btn-regresar-API";
   const textBoton = document.createTextNode("Regresar");
   btnRegresar.appendChild(textBoton)
   return btnRegresar
