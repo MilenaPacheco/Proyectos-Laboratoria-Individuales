f8b3808f34d3c9889924863c5425ef916f8d905b
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2024-02-11 23:35:39 -0500

    Se agrega texto al boton haciendo uso de Nodos

--- src/components/btnChat.js
+++ src/components/btnChat.js
@@ -1,5 +1,7 @@
 export const btnChat = () => {
   const btnChat = document.createElement("button");
   btnChat.className = "btn-chat";
+  const textBoton = document.createTextNode("Chatear");
+  btnChat.appendChild(textBoton);
   return btnChat;
 };
