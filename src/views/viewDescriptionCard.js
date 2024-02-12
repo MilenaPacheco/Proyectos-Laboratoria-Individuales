16f8983d9d9df888cc77042fd615c596cb59e4af
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2024-02-11 23:39:07 -0500

    Se agregan nuevas clases y un div en las listas con el fin de que el estilo de estas no se vean afectadas por los estilos establecidos principalmente

--- src/views/viewDescriptionCard.js
+++ src/views/viewDescriptionCard.js
@@ -42,10 +42,10 @@ export const viewDescriptionCard = (cardId) => {
   botonRegresar.addEventListener("click", () => {
     navigateTo(`/`)
   });
-  /*const btnChatgrupal = document.querySelector(".btn-chat-grupal");
-  btnChatgrupal.addEventListener("click", () => {
+  //const btnChatgrupal = document.querySelector(".btn-chat-grupal");
+  botonChat.addEventListener("click", () => {
     navigateTo(`/viewChats`);
-  });*/
+  });
   //console.log(root)
   return root 
 };
@@ -65,8 +65,7 @@ function descriptionCard(film){
   const html = ` 
         <li class="card-description-short" itemscope itemtype="movie">
         <dl>
-          <dt></dt><dd class="name-movie" itemprop="name"><strong>${film.name}</strong></dd>
-          <dt></dt><dd class="year-movie" itemprop="yearMovie"><strong>(${film.facts.yearMovie})</strong></dd>
+          <dt></dt><dd class="name-movie-description" itemprop="name"><strong>${film.name}</strong></dd>
           <dt></dt><dd class= "info-movie" itemprop="shortDescription">${film.shortDescription}</dd>
           <dt></dt><dd itemprop="genreMovie"><strong>Género: </strong>${film.facts.genreMovie}</dd>
         </dl>
@@ -76,25 +75,39 @@ function descriptionCard(film){
   return descriptionCardHTML
 }
 
-function descriptionCard2(film){
-  const descriptionCardHTML = document.createElement("ul");
-  descriptionCardHTML.className = "list-description-all"
-  const html = `<li class="card-description-all" itemscope itemtype="movie">
+function descriptionCard2(film) {
+  const descriptionCardHTML = document.createElement("div");
+  descriptionCardHTML.className = "description-card";
+
+  // Primera lista
+  const list1 = document.createElement("ul");
+  list1.className = "list-description-all";
+  list1.innerHTML = `
+    <li class="card-description-all" itemscope itemtype="movie">
       <dl>
-        <dt></dt><dd class="year-movie" itemprop="yearMovie"><strong>Año de lanzamiento: (${film.facts.yearMovie})</strong></dd>
-        <dt></dt><dd class= "director-movie" itemprop="directorMovie"><strong>Director: ${film.facts.directorMovie}</dd>
+        <dt></dt><dd class="year-movie-description" itemprop="yearMovie"><strong>Año de lanzamiento: </strong>${film.facts.yearMovie}</dd>
+        <dt></dt><dd class= "director-movie" itemprop="directorMovie"><strong>Director: ${film.facts.directorMovie}</strong></dd>
         <dt></dt><dd itemprop="productionCompany"><strong>Compañia de Producción: </strong>${film.facts.productionCompany}</dd>
         <dt></dt><dd itemprop="rottenTomatoesScore"><strong>Puntuación RottennTomatoes: </strong>${film.extraInfo.rottenTomatoesScore}</dd>
-        <dt></dt><dd itemprop="countryMovie"><País: </strong>${film.extraInfo.countryMovie}</dd>
-        <dt></dt><dd itemprop="durationMovie"><Duración: </strong>${film.extraInfo.durationMovie}</dd>
+        <dt></dt><dd itemprop="countryMovie"><strong>País: </strong>${film.extraInfo.countryMovie}</dd>
+        <dt></dt><dd itemprop="durationMovie"><strong>Duración: </strong>${film.extraInfo.durationMovie}</dd>
       </dl>
     </li>
-    <li class="cards" itemscope itemtype="movie">
+  `;
+
+  // Segunda lista
+  const list2 = document.createElement("p");
+  list2.className = "list-description-all";
+  list2.innerHTML = `
+    
       <dl>
-      <dt></dt><dd class= "description" itemprop="description"><strong>Descripción:  ${film.description}</dd>
+        <dt></dt><dd class= "description" itemprop="description"><strong>Descripción: </strong><br>${film.description}</dd>
       </dl>
-    </li>
-    `;
-  descriptionCardHTML.innerHTML = html; 
-  return descriptionCardHTML
+  `;
+  // Agregar las listas al contenedor principal
+  descriptionCardHTML.appendChild(list1);
+  descriptionCardHTML.appendChild(list2);
+
+  return descriptionCardHTML;
 }
+
